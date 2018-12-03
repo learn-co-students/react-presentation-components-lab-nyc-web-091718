0cf0a9c468381c4c7804e5e1345573056cd7d538
asc5025 <albert.s.chun@gmail.com>
2018-12-03 08:23:40 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+const SimplerComponent = ({handleClick}) => {
+  return <div onClick={handleClick}>I am just happy</div>
+
+} //end of functional Component
+
+
+export default SimplerComponent;
